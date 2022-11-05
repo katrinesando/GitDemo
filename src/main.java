635cf6a565fa67f909fe04ebd44559d7cec06e77
katrinesando <kmsa@itu.dk>
2022-11-05 17:49:26 +0100

    branch

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("This is a Git Demo");
+        System.out.println("This is a Git Demo on a new Branch");
+        System.out.println("This is not in main");
     }
 }
