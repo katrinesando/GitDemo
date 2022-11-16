0c5e3eff07eb4e1a9b6311404bbdb4c485ed9107
Oliver Flyckt Wilhjelm <olfw@itu.dk>
2022-11-16 19:30:34 +0100

    added parts for a merge conflix

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("This is a Git Demo");
+
+
+        System.out.println("This is another part of a Git Demo");
     }
 }
