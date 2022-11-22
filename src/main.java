d628ddebf2687eaa0911b4deb2681758c9691dc4
katrinesando <kmsa@itu.dk>
2022-11-22 12:39:35 +0100

    beginning to implement forks and security token in updated blazor

--- src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("This is a Git Demo on a new Branch");
-        System.out.println("This is not in main");
-        System.out.println("This is a Git Demo");
-        System.out.println("This is another part of a Git Demo");1
-    }
-}
